7609860616cd96abdb2a6edfd933d1c87a39d648
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-12-28 15:23:33 -0500

    initial commit of sources inside '.spring-init/kafka-inspect--maven.zip' from 'https://start.spring.io/'

---
+++ src/main/java/io/github/krmdemo/kafkainspect/KafkaInspectApplication.java
@@ -0,0 +1,13 @@
+package io.github.krmdemo.kafkainspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaInspectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/krmdemo/kafkainspect/KafkaInspectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.krmdemo.kafkainspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaInspectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
