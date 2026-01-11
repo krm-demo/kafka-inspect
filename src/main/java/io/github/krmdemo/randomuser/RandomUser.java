d1ee722482e1133c20621bae82aa427c33ab9929
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2026-01-11 17:31:04 -0500

    continue playing with random-users data-classes #2

--- src/main/java/io/github/krmdemo/randomuser/RandomUser.java
+++ src/main/java/io/github/krmdemo/randomuser/RandomUser.java
@@ -1,16 +1,20 @@
 package io.github.krmdemo.randomuser;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
 import java.time.Instant;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * TODO: debug and test ser/deser according to <a href="https://randomuser.me/api/">Random-User API</a>
+ * The data-class represents all hierarchical properties of <b>Random-User</b>
+ *
+ * @see  <a href="https://randomuser.me/api/">Random-User API</a>
  */
 @Data
 public class RandomUser {
@@ -19,7 +23,14 @@ public class RandomUser {
     private Name name;
     private Location location;
     private String email;
-    @JsonProperty("dob") private DateOfBirth dateOfBirth;
+    @JsonProperty("login") Credentials credentials;
+    @JsonProperty("dob") private DateAge dateOfBirth;
+    @JsonProperty("registered") private DateAge registeredAt;
+    @JsonProperty("phone") private String phoneNumber;
+    @JsonProperty("cell") private String cellPhoneNum;
+    NameValue id;
+    @JsonProperty("picture") PictureLinks pictureLnks;
+    @JsonProperty("nat") String nationality;
 
     public enum Gender {
         MALE("male"),
@@ -52,7 +63,10 @@ public class RandomUser {
         @JsonProperty("street") StreetHouse streetHouse,
         String city,
         String country,
-        Integer postalCode
+        String state,
+        Integer postcode,
+        GeoCoordinates coordinates,
+        TimeZone timezone
     ) {}
 
     public record StreetHouse(
@@ -60,8 +74,46 @@ public class RandomUser {
         @JsonProperty("name") String street
     ) {}
 
-    public record DateOfBirth(
+    public record GeoCoordinates(
+        Double latitude,  // TODO: try to address trailing zero using << pattern = "#.####" >>
+        Double longitude
+    ) {}
+
+    public record TimeZone(
+        String offset,
+        String description
+    ) {
+        // TODO: it make sense to add some useful methods to get the native Java DateTime values
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")  // <-- IDEA does not like date-time-format spelling
+    public record DateAge(
+        @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",  // <-- 'T' and 'Z' are surraounded with single quotes!
+            timezone = "UTC")
         Instant date,
         Integer age
+    ) {}
+
+    public record NameValue(
+        String name,
+        String value
+    ) {}
+
+    public record PictureLinks(
+        String large,
+        String medium,
+        String thumbnail
+    ) {}
+
+    public record Credentials(
+        UUID uuid,
+        String username,
+        String password,
+        String salt,
+        String md5,
+        String sha1,
+        String sha256
     ) {}
 }
--- src/test/java/io/github/krmdemo/randomuser/RandomUserTest.java
+++ src/test/java/io/github/krmdemo/randomuser/RandomUserTest.java
@@ -1,18 +1,63 @@
 package io.github.krmdemo.randomuser;
 
+import org.json.JSONException;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.krmdemo.techlabs.core.dump.DumpUtils;
+import org.krmdemo.techlabs.core.utils.CoreResourceUtils;
 import org.krmdemo.techlabs.core.utils.JacksonUtils;
 
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * This unit-test verifies serialization and de-serialization abilities of data-class {@link RandomUser}
+ *
+ * @see <a href="https://github.com/lukas-krecan/JsonUnit">json-unit-assertj</a>
+ */
 public class RandomUserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
-        "unit-tests/sample-user--Abel-Moreau.json"
+        "unit-tests/sample-user--Abel-Moreau.json",
+        "unit-tests/sample-user--Dya-XXX.json",      // <-- seed: "00fdd9bf0ac99b24"
+        "unit-tests/sample-user--Adam-Morris.json"   // <-- seed: "215b765ce74a2f9e"
     })
-    void testRandomUser(String resourcePath) {
+    void testWholeJSON(String resourcePath) throws JSONException {
+        String originalJson = CoreResourceUtils.resourceAsText(resourcePath);
         RandomUser randomUser = JacksonUtils.jsonValueFromResource(resourcePath, RandomUser.class);
-        System.out.println("randomUser --> " + DumpUtils.dumpAsJsonTxt(randomUser));
+        String dumpedJson = DumpUtils.dumpAsJsonTxt(randomUser);
+
+        // assertThat(dumpedJson).isEqualTo(originalJson);  // <-- does not work !!!
+        // assertThat(dumpedJson).isEqualToIgnoringWhitespace(originalJson);  // <-- it's not enough
+
+        // An example of lenient JSON-comparison using https://github.com/skyscreamer/JSONassert
+        //JSONAssert.assertEquals(originalJson, dumpedJson, JSONCompareMode.LENIENT);  // <-- problems with integers
+
+        // An example of lenient JSON-comparison using https://github.com/lukas-krecan/JsonUnit?tab=readme-ov-file#features
+        assertThatJson(dumpedJson)
+            .whenIgnoringPaths(
+                "dob.age",
+                "location.postcode",
+                "location.street.number",
+                "registered.age")
+            .isEqualTo(originalJson);
+        // TODO: investigate this and abilities of <<...and( a -> assertThat(a.node("id")...)...>>
+    }
+
+    @ParameterizedTest(name = "[{index}] check some properties of {1}")
+    @CsvSource({
+        "unit-tests/sample-user--Abel-Moreau.json, 'Mr Abel Moreau', 77, 64573, 3175, 7",
+        "unit-tests/sample-user--Dya-XXX.json, 'Miss Oya Topaloğlu', 27, 65290, 6055, 23",
+        "unit-tests/sample-user--Adam-Morris.json, 'Mr Adam Morris', 64, 24705, 6533, 18"
+    })
+    void testSomeProps(String resourcePath, String fullName, int ageBirth, int postcode, int streetNum, int ageReg) {
+        RandomUser randomUser = JacksonUtils.jsonValueFromResource(resourcePath, RandomUser.class);
+        assertThat(randomUser.getName().full()).isEqualTo(fullName);
+        assertThat(randomUser.getDateOfBirth().age()).isEqualTo(ageBirth);
+        assertThat(randomUser.getLocation().postcode()).isEqualTo(postcode);
+        assertThat(randomUser.getLocation().streetHouse().number()).isEqualTo(streetNum);
+        assertThat(randomUser.getRegisteredAt().age()).isEqualTo(ageReg);
     }
 }
